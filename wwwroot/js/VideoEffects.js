971041e6cda5c205ff8522ea8d8749a11b61d515
donutz03 <ionut.hodoroaga20@gmail.com>
2025-02-22 10:55:00 +0200

    try integrating video in canvas

--- wwwroot/js/VideoEffects.js
+++ wwwroot/js/VideoEffects.js
@@ -5,62 +5,55 @@ class VideoEffects {
         this.ctx = this.canvas.getContext('2d');
         this.currentEffect = 0;
         this.isPlaying = false;
+        this.showControls = false;
 
-        this.setupCanvas();
+        this.progressBarHeight = 30;
+        this.controlButtonWidth = 40;
+
         this.setupEventListeners();
-        this.setupControls();
-    }
-
-    setupCanvas() {
-        this.video.addEventListener('loadedmetadata', () => {
-            const videoRect = this.video.getBoundingClientRect();
-            this.canvas.width = this.video.videoWidth;
-            this.canvas.height = this.video.videoHeight;
-            this.canvas.style.width = videoRect.width + 'px';
-            this.canvas.style.height = videoRect.height + 'px';
-        });
+        this.draw();
     }
 
     setupEventListeners() {
+        this.canvas.addEventListener('mouseenter', () => {
+            this.showControls = true;
+        });
+
+        this.canvas.addEventListener('mouseleave', () => {
+            this.showControls = false;
+        });
+
+        this.canvas.addEventListener('click', (e) => {
+            const rect = this.canvas.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            const canvasHeight = this.canvas.height;
+
+            if (y > canvasHeight - this.progressBarHeight) {
+                const clickProgress = x / this.canvas.width;
+                this.video.currentTime = clickProgress * this.video.duration;
+            }
+            else {
+                if (this.video.paused) {
+                    this.video.play();
+                    this.isPlaying = true;
+                } else {
+                    this.video.pause();
+                    this.isPlaying = false;
+                }
+            }
+        });
+
         this.video.addEventListener('play', () => {
             this.isPlaying = true;
-            this.render();
         });
 
         this.video.addEventListener('pause', () => {
             this.isPlaying = false;
         });
-
-        this.video.addEventListener('ended', () => {
-            this.isPlaying = false;
-        });
-
-        this.canvas.addEventListener('click', () => {
-            if (this.video.paused) {
-                this.video.play();
-            } else {
-                this.video.pause();
-            }
-        });
     }
 
-    setupControls() {
-        this.canvas.style.cursor = 'pointer';
-
-        this.video.addEventListener('canplay', () => {
-            if (this.isPlaying) {
-                this.video.play().catch(e => console.log('Error playing video:', e));
-            }
-        });
-    }
-
-    setEffect(effectId) {
-        this.currentEffect = effectId;
-    }
-
-    render() {
-        if (!this.isPlaying) return;
-
+    draw() {
         this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
 
         if (this.currentEffect > 0) {
@@ -68,23 +61,61 @@ class VideoEffects {
             const pixels = imageData.data;
 
             switch (this.currentEffect) {
-                case 1: 
+                case 1:
                     this.applyGrayscale(pixels);
                     break;
-                case 2: 
+                case 2:
                     this.applySepia(pixels);
                     break;
-                case 3: 
+                case 3:
                     this.applyInvert(pixels);
                     break;
             }
 
-
             this.ctx.putImageData(imageData, 0, 0);
-            
         }
 
-        requestAnimationFrame(() => this.render());
+        if (this.showControls) {
+            this.drawControls();
+        }
+
+        requestAnimationFrame(() => this.draw());
+    }
+
+    drawControls() {
+        const width = this.canvas.width;
+        const height = this.canvas.height;
+
+        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        this.ctx.fillRect(0, height - this.progressBarHeight, width, this.progressBarHeight);
+
+        if (this.video.duration) {
+            const progress = (this.video.currentTime / this.video.duration) * width;
+            this.ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+            this.ctx.fillRect(0, height - this.progressBarHeight, progress, this.progressBarHeight);
+        }
+
+        this.ctx.fillStyle = 'white';
+        this.ctx.font = '14px Arial';
+        this.ctx.textAlign = 'right';
+        const timeText = `${Math.floor(this.video.currentTime)}s / ${Math.floor(this.video.duration)}s`;
+        this.ctx.fillText(timeText, width - 10, height - this.progressBarHeight/2);
+
+        this.ctx.fillStyle = 'white';
+        if (this.video.paused) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(10, height - this.progressBarHeight/2 - 10);
+            this.ctx.lineTo(30, height - this.progressBarHeight/2);
+            this.ctx.lineTo(10, height - this.progressBarHeight/2 + 10);
+            this.ctx.fill();
+        } else {
+            this.ctx.fillRect(10, height - this.progressBarHeight/2 - 10, 5, 20);
+            this.ctx.fillRect(20, height - this.progressBarHeight/2 - 10, 5, 20);
+        }
+    }
+
+    setEffect(effectId) {
+        this.currentEffect = effectId;
     }
 
     applyGrayscale(pixels) {
